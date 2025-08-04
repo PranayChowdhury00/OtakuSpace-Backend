66ea992782332b02fa582c0dbbbbb2a6f1e33a67
PranayChowdhury00 <pranaychowdhury00@gmail.com>
2025-08-05 00:12:45 +0600

    url

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors(
     {
-        origin: ['http://localhost:5173'], //replace with client address
+        origin: ['http://localhost:5173','https://porleiviral.web.app/'], //replace with client address
         credentials: true,
     }
 )); 
